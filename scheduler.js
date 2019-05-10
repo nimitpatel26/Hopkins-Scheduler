dc71dadea78099465da1cb9e7a5d107163cc6547
jamesseeman <jamesseeman@protonmail.com>
2019-05-09 22:05:52 -0400

    Added download
    Can download the settings as a json

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,18 @@
+//This function is used to download the json file
+//Source: https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
 $('document').ready(function() {
 	//Add new row once input is added
 	$('.form').on('change keyup', '.first', function(e) {
@@ -41,5 +56,48 @@ $('document').ready(function() {
 			str += vals;
 			$(this).html(str);
 		});
+	});
+
+	$('#download-btn').click(function() {
+		var settingsObj = {};
+
+		settingsObj.locations = [];
+		$('#locations input').each(function() {
+			if($(this).val())
+				settingsObj.locations.push($(this).val());
+		});
+		
+		settingsObj.personTypes = [];
+		$('#personTypes .row').each(function() {
+			if($(this).find('.first').val()) {
+				var name = $(this).find('.first').val();
+				var reqHours = $(this).find('.reqHours').val();
+				var location = $(this).find('.locationName').val();
+				settingsObj.personTypes.push([name, reqHours, location]);
+			}
+		});
+
+		settingsObj.people = [];
+		$('#people .row').each(function() {
+			if($(this).find('.first').val()) {
+				var name = $(this).find('.first').val();
+				var type = $(this).find('.myType').val();
+				var workedHours = $(this).find('.workedHours').val();
+				settingsObj.people.push([name, type, workedHours]);
+			}
+		});	
+
+		settingsObj.shiftTypes = [];
+		$('#shiftTypes .row').each(function() {
+			var first = $(this).find('.first');
+			if(first.val() != first.attr('data-default')) {
+				var length = first.val();
+				var type = $(this).find('.myType').val();
+				var dates = $(this).find('.dates').val();
+				settingsObj.shiftTypes.push([length, type, dates]);
+			}
+		});
+		
+		download("scheduler_settings.json", JSON.stringify(settingsObj));
 	});
 });
